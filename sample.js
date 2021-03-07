5b69fef55bb0568fc82dbf4f93cca05467f4f2a7
Eric Volpert <ericvolp12@gmail.com>
2021-03-06 22:54:19 -0800

    Fridge outlet control via lights API

--- sample.js
+++ sample.js
@@ -1,45 +1,79 @@
-const IBS_TH1 = require('./ibs_th1');
-const log4js = require('log4js')
+const IBS_TH1 = require("./ibs_th1");
+const log4js = require("log4js");
 
 const logger_ = log4js.getLogger();
-logger_.level = 'info';
+logger_.level = "info";
 
 const device = new IBS_TH1();
-
 
 let state = {
   connecting: false,
   gotData: false,
-  waitCount: 60
-}
+  waitCount: 60,
+  triggerTemp: 20,
+  triggerMinutes: 5,
+  lastPowerReset: new Date(0),
+  cooldownMin: 60,
+  powerCycleWaitTime: 15,
+};
 
 let sensorState = {
   temps: [],
   battery: 0,
-  lastUpdated: new Date().toString('en-US')
-}
+  lastUpdated: new Date().toString("en-US"),
+};
 
-
-const callback = data => {
-  logger_.debug(`Current Temp: ${data.temperature.toFixed(2)}°F -` +
-    ` Battery: ${data.battery}%`);
+const callback = (data) => {
+  logger_.debug(
+    `Current Temp: ${data.temperature.toFixed(2)}°F -` +
+      ` Battery: ${data.battery}%`
+  );
   sensorState.battery = data.battery;
-  if(sensorState.temps.length > 60){
+  if (sensorState.temps.length > 60) {
     sensorState.temps.shift();
   }
   sensorState.temps.push(data.temperature.toFixed(3));
-  sensorState.lastUpdated = new Date().toString('en-US');
+  sensorState.lastUpdated = new Date().toString("en-US");
 
   state.gotData = true;
   device.unsubscribeRealtimeData();
-  logger_.debug('Disconnecting from sensor');
+  logger_.debug("Disconnecting from sensor");
 };
 
-
 const dutyLoop = function () {
+  // Evaluate state
+  const now = new Date();
+  if (now - state.lastPowerReset > state.cooldownMin * 60000) {
+    if (
+      sensorState.temps
+        .slice(-state.triggerMinutes)
+        .filter((temp) => temp > state.triggerTemp).length >=
+      state.triggerMinutes
+    ) {
+      logger_.info(
+        `Freezer has been over trigger temp of (${state.triggerTemp}F) for at least (${state.triggerMinutes}) minutes.`
+      );
+      if (now - state.lastPowerReset > state.triggerMinutes * 60000) {
+        logger_.info(
+          `Resetting fridge power, last reset was at (${state.lastPowerReset.toLocaleString(
+            "en-US"
+          )})`
+        );
+        state.lastPowerReset = now;
+        setFridgeState("off");
+        setTimeout(() => {
+          logger_.info(
+            `Powering fridge back on after (${state.powerCycleWaitTime}) minutes...`
+          );
+          setFridgeState("on");
+        }, state.powerCycleWaitTime * 60000);
+      }
+    }
+  }
+
   if (!state.connecting && state.waitCount >= 60) {
     device.subscribeRealtimeData(callback);
-    logger_.debug('Connecting to sensor');
+    logger_.debug("Connecting to sensor");
     state.connecting = true;
   } else if (!state.connecting) {
     state.waitCount++;
@@ -50,27 +84,40 @@ const dutyLoop = function () {
       state.connecting = false;
     }
   }
-}
+};
 
-/*
-const report = function(){
-  logger_.info(`Sensor State:\n\tTemps: ${sensorState.temps.map(temp => temp.toFixed(2))}\n\tBattery: ${sensorState.battery}`);
-}
-*/
+const axios = require("axios");
 
-const express = require('express')
-const app = express()
-const port = 3000
+const setFridgeState = function (desiredState) {
+  axios
+    .post(process.env.OUTLET_URL, {
+      apiKey: process.env.OUTLET_API_KEY,
+      device: process.env.OUTLET_DEVICE_NAME,
+      powerState: desiredState,
+    })
+    .then((res) => {
+      logger_.info(
+        `Successfully set fridge to state (${desiredState}): `,
+        res.data
+      );
+    })
+    .catch((err) => {
+      logger_.error(`Failed to set fridge to state (${desiredState}): `, err);
+    });
+};
 
-app.get('/sensor_state', (req, res) => {
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/sensor_state", (req, res) => {
   res.json(sensorState);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://10.0.7.5:${port}`);
+  console.log(`Fridgemon API listening at http://10.0.7.5:${port}`);
 });
 
 logger_.info("Starting duty loop...");
 setInterval(dutyLoop, 1000);
-//setInterval(report, 10000);
-
