618ad9b0b6b64f8760aeff89b9a545d61ff32432
Eric Volpert <ericvolp12@gmail.com>
2021-03-08 19:04:52 -0800

    Prometheus metrics because why not...

--- sample.js
+++ sample.js
@@ -1,26 +1,47 @@
-const IBS_TH1 = require("./ibs_th1");
-const log4js = require("log4js");
+const IBS_TH1 = require('./ibs_th1');
+const log4js = require('log4js');
 
 const logger_ = log4js.getLogger();
-logger_.level = "info";
+logger_.level = 'info';
 
 const device = new IBS_TH1();
+
+const promClient = require('prom-client');
+const tempPointsCaptured = new promClient.Counter({
+  name: 'temperature_points_captured',
+  help: 'Number of temperature datapoints captured from the sensor',
+});
+
+const fridgeResets = new promClient.Counter({
+  name: 'fridge_resets',
+  help: 'Number of times the fridge has had its power reset',
+});
+
+const tempGauge = new promClient.Gauge({
+  name: 'current_temperature',
+  help: 'Current freezer temperature from the sensor',
+});
 
 let state = {
   connecting: false,
   gotData: false,
   waitCount: 60,
-  triggerTemp: 20,
+  triggerTemp: 25,
   triggerMinutes: 5,
-  lastPowerReset: new Date(0),
-  cooldownMin: 60,
+  powerResets: [
+    {
+      time: new Date(0),
+      timeReadable: new Date(0).toLocaleString('en-US'),
+    },
+  ],
+  cooldownMin: 90,
   powerCycleWaitTime: 15,
 };
 
 let sensorState = {
   temps: [],
   battery: 0,
-  lastUpdated: new Date().toString("en-US"),
+  lastUpdated: new Date().toLocaleString('en-US'),
 };
 
 const callback = (data) => {
@@ -33,17 +54,19 @@ const callback = (data) => {
     sensorState.temps.shift();
   }
   sensorState.temps.push(data.temperature.toFixed(3));
-  sensorState.lastUpdated = new Date().toString("en-US");
+  sensorState.lastUpdated = new Date().toLocaleString('en-US');
+  tempPointsCaptured.inc();
+  tempGauge.set(Number(data.temperature.toFixed(3)));
 
   state.gotData = true;
   device.unsubscribeRealtimeData();
-  logger_.debug("Disconnecting from sensor");
+  logger_.debug('Disconnecting from sensor');
 };
 
 const dutyLoop = function () {
   // Evaluate state
   const now = new Date();
-  if (now - state.lastPowerReset > state.cooldownMin * 60000) {
+  if (now - state.powerResets.slice(-1).time > state.cooldownMin * 60000) {
     if (
       sensorState.temps
         .slice(-state.triggerMinutes)
@@ -53,19 +76,29 @@ const dutyLoop = function () {
       logger_.info(
         `Freezer has been over trigger temp of (${state.triggerTemp}F) for at least (${state.triggerMinutes}) minutes.`
       );
-      if (now - state.lastPowerReset > state.triggerMinutes * 60000) {
+      if (
+        now - state.powerResets.slice(-1).time >
+        state.triggerMinutes * 60000
+      ) {
         logger_.info(
-          `Resetting fridge power, last reset was at (${state.lastPowerReset.toLocaleString(
-            "en-US"
-          )})`
+          `Resetting fridge power, last reset was at (${
+            state.powerResets.slice(-1).timeReadable
+          })`
         );
-        state.lastPowerReset = now;
-        setFridgeState("off");
+        if (state.powerResets.length > 1000) {
+          state.powerResets.shift();
+        }
+        state.powerResets.push({
+          time: now,
+          timeReadable: now.toString('en-US'),
+        });
+        fridgeResets.inc();
+        setFridgeState('off');
         setTimeout(() => {
           logger_.info(
             `Powering fridge back on after (${state.powerCycleWaitTime}) minutes...`
           );
-          setFridgeState("on");
+          setFridgeState('on');
         }, state.powerCycleWaitTime * 60000);
       }
     }
@@ -73,7 +106,7 @@ const dutyLoop = function () {
 
   if (!state.connecting && state.waitCount >= 60) {
     device.subscribeRealtimeData(callback);
-    logger_.debug("Connecting to sensor");
+    logger_.debug('Connecting to sensor');
     state.connecting = true;
   } else if (!state.connecting) {
     state.waitCount++;
@@ -86,7 +119,7 @@ const dutyLoop = function () {
   }
 };
 
-const axios = require("axios");
+const axios = require('axios');
 
 const setFridgeState = function (desiredState) {
   axios
@@ -106,21 +139,29 @@ const setFridgeState = function (desiredState) {
     });
 };
 
-
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/sensor_state", (req, res) => {
+app.get('/sensor_state', (req, res) => {
   res.json({
     sensorState,
-    state
+    state,
   });
 });
 
+app.get('/metrics', (req, res) => {
+  logger_.debug("Prometheus metrics scraped...");
+  promClient.register.metrics().then(metrics => {
+    res.send(metrics);
+  }).catch(err =>{
+    res.err(err);
+  });
+})
+
 app.listen(port, () => {
-  console.log(`Fridgemon API listening at http://10.0.7.5:${port}`);
+  logger_.info(`Fridgemon API listening at http://0.0.0.0:${port}`);
 });
 
-logger_.info("Starting duty loop...");
+logger_.info('Starting duty loop...');
 setInterval(dutyLoop, 1000);
